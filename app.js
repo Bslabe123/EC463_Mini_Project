acff2c3c733aede8bf85701002b4e0c2e9ddf99c
Saahil <saahils365@gmail.com>
2020-09-13 17:15:35 -0400

    added a function where a user's unique google ID is stored to a json file when they log in

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require("express");
 var app = express();
 var request = require("request");
 const path = require('path');
+var bodyParser = require("body-parser");
+var userID = "0";
+var fs = require("fs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 //app.use(express.static(path.join(__dirname, 'views')));
 
@@ -13,6 +19,23 @@ app.get("/loginPage",function(req, res) {
 	res.sendFile("C:/Users/Owner/Desktop/MiniProject/views/loggedOut.html");
 })
 
+app.post("/register", function(req, res) {
+	console.log(req.body);
+	var partialID = req.body.theName.substring(0,30);
+	var obj = {
+		table: []
+	};
+	obj.table.push({id: partialID, data: [["bedroom",1,2,3]]});
+	var json = JSON.stringify(obj);
+	fs.writeFile("./userData.json",json,function(err) {
+	if (err) {
+        console.log('Error writing file', err)
+    } else {
+        console.log('Successfully wrote file')
+    }
+	});
+	res.redirect("/loggedIn");
+})
 
 app.get("/loggedIn",function(req,res) {
 	res.sendFile("C:/Users/Owner/Desktop/MiniProject/views/loggedIn.html");
