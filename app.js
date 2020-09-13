d79ca352181ea4b8e679b871f7c646d0b7b5afd1
Saahil <saahils365@gmail.com>
2020-09-13 01:09:29 -0400

    added a login page and a data page, users are able to go to the data page if logged in and log out if in the data page

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+const path = require('path');
 
-app.get("/",function(req, res) {
-	res.render("homePage.ejs");
+//app.use(express.static(path.join(__dirname, 'views')));
+
+app.get("/",function(req,res) {
+	res.redirect("/loginPage")
 })
 
+app.get("/loginPage",function(req, res) {
+	res.sendFile("C:/Users/Owner/Desktop/MiniProject/views/loggedOut.html");
+})
+
+
 app.get("/loggedIn",function(req,res) {
-	res.render("loggedIn.ejs")
+	res.sendFile("C:/Users/Owner/Desktop/MiniProject/views/loggedIn.html");
 })
 
 app.listen(8000, function() {
